aaeeab2d43e9c6101449cca118f39a64f5b832fb
Chanakan-Ponpueng <chanakan.p@arise.tech>
2024-05-18 22:54:47 +0700

    dev TestFizzBuzzShouldReturnBuzzWhenInput5

--- testgo/fizzbuzz.go
+++ testgo/fizzbuzz.go
@@ -2,9 +2,15 @@ package main
 
 import "fmt"
 
+var fizzMap map[bool]string = map[bool]string{true: "Fizz", false: ""}
+var buzzMap map[bool]string = map[bool]string{true: "Buzz", false: ""}
+
 func FizzBuzz(n int) string {
-	if n == 3 {
-		return "Fizz"
+	result := ""
+	result += fizzMap[n%3 == 0]
+	result += buzzMap[n%5 == 0]
+	if result == "" {
+		return fmt.Sprintf("%d", n)
 	}
-	return fmt.Sprintf("%d", n)
+	return result
 }
