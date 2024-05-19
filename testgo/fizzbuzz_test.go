29cd013690e9d10af7b06cd79d93068a62f4c26b
Chanakan-Ponpueng <chanakan.p@arise.tech>
2024-05-19 14:11:14 +0700

    TestFizzBuzzShouldReturnFizzBuzzWhenInput15

--- testgo/fizzbuzz_test.go
+++ testgo/fizzbuzz_test.go
@@ -150,7 +150,7 @@ func TestFizzBuzzShouldReturn14WhenInput14(t *testing.T) {
 	}
 }
 
-func TestFizzBuzzShouldReturnFizzWhenInputBuzz(t *testing.T) {
+func TestFizzBuzzShouldReturnFizzBuzzWhenInput15(t *testing.T) {
 	input := 15
 	got := FizzBuzz(input)
 
