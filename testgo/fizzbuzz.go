e4bebe9f6a5097d8d1c8e2d8c5c9fff19a80cc46
Chanakan-Ponpueng <chanakan.p@arise.tech>
2024-05-23 20:42:02 +0700

    dev TestFizzBuzzShouldReturnBuzzWhenInput10

--- testgo/fizzbuzz.go
+++ testgo/fizzbuzz.go
@@ -12,7 +12,7 @@ func FizzBuzz(n int) string {
 		true : "Buzz", 
 		false : "",
 	}
-	result := fmt.Sprintf("%s%s",fizzMap[n%3 ==0 ],buzzMap[n==5])
+	result := fmt.Sprintf("%s%s",fizzMap[n%3 ==0 ],buzzMap[n%5 == 0 ])
 	resultMap := map[bool]string{
 		true: fmt.Sprintf("%d",n),
 		false: result,
