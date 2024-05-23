6e3ec6ef73890f445de463b6be385790b92311c2
Chanakan-Ponpueng <chanakan.p@arise.tech>
2024-05-23 20:23:24 +0700

    add test TestFizzBuzzShouldReturn4WhenInput4

--- testgo/fizzbuzz_test.go
+++ testgo/fizzbuzz_test.go
@@ -34,3 +34,14 @@ func TestFizzBuzzShouldReturnFizzWhenInput3(t *testing.T) {
 		t.Errorf("got %q but want %q", got, want)
 	}
 }
+
+func TestFizzBuzzShouldReturn4WhenInput4(t *testing.T) {
+	input := 3
+
+	got := FizzBuzz(input)
+
+	want := "Fizz"
+	if got != want {
+		t.Errorf("got %q but want %q", got, want)
+	}
+}
